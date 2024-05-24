ed7f50b53e705669b19282ad368951d44229b527
PopeyedBird6 <isaac45604@gmail.com>
2024-05-23 23:52:32 -0600

    Gordozzzzz

--- app/_components/SpFooter.jsx
+++ app/_components/SpFooter.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
 
 
-    <footer className="bg-secundary rounded-lg shadow m-4 dark:bg-gray-800">
+    <footer className="bg-secundary rounded-lg shadow m-2 dark:bg-gray-800">
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <Link href="/" className="hover:underline">Pardalis™</Link>
         </span>
--- app/terms/page.jsx
+++ app/terms/page.jsx
@@ -6,8 +6,8 @@ export default function Page() {
         <div className="min-h-screen flex flex-col fondo">
             <NavBar />
 
-            <main className="grid grid-cols-2 flex-1 p-4">
-                <section className="grid place-content-center p-6 bg-background rounded-lg hover:bg-background dark:bg-background m-4">
+            <main className="grid center flex-1 p-4">
+                <section className="grid place-content-center p-6 bg-background rounded-lg hover:bg-background dark:bg-background m-1">
                     <h2 className="text-center text-4xl font-extrabold">¿Qué es Pardalis?</h2>
                     <p className="m-2 mt-5">
                         El equipo de desarrollo, denominado Ponchoides, presenta la propuesta de la aplicación web Pardalis
@@ -15,18 +15,6 @@ export default function Page() {
                         específico en aquellos que se encuentran en el rango de edades de 6 a 9 años, durante su proceso de
                         aprendizaje del idioma inglés. Pardalis se distingue por ofrecer una experiencia de aprendizaje única y
                         natural, donde los niños pueden desarrollar sus habilidades lingüísticas de manera lúdica y efectiva.
-                    </p>
-                </section>
-
-                <section className="grid place-content-center p-6 bg-background rounded-lg hover:bg-background dark:bg-background m-4">
-                    <h2 className="text-center text-4xl font-extrabold">¿Por qué elegir Pardalis?</h2>
-                    <p className="m-2 mt-5">
-                        La aplicación está diseñada con una interfaz amigable y atractiva, adaptada a las necesidades y preferencias
-                        de los usuarios más jóvenes, con el objetivo de convertir el aprendizaje del inglés en una experiencia divertida
-                        y motivadora. Pardalis integra diversos recursos didácticos interactivos, juegos educativos y actividades
-                        dinámicas que buscan estimular el interés y la participación de los niños, favoreciendo así un aprendizaje más
-                        significativo.
-                        
                     </p>
                 </section>
             </main>
