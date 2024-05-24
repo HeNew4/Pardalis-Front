6494d33ae9f070e4901b8166974cbc9db3d872f7
PopeyedBird6 <isaac45604@gmail.com>
2024-05-24 00:11:39 -0600

    terms

--- app/terms/page.jsx
+++ app/terms/page.jsx
@@ -8,8 +8,11 @@ export default function Page() {
 
             <main className="grid center flex-1 p-4">
                 <section className="grid place-content-center p-6 bg-background rounded-lg hover:bg-background dark:bg-background m-1">
-                    <h2 className="text-center text-4xl font-extrabold">¿Qué es Pardalis?</h2>
-                    <p className="m-2 mt-5">
+                    <h2 className=" text-4xl font-extrabold ml-2">Términos y Condiciones</h2>
+                    <p className="ml-3">Actualizado el 2024-05-23</p>
+                    <h2 className=" text-4xl font-extrabold m-2">Términos generales</h2>
+
+                    <p className="m-3 mt-2">
                         El equipo de desarrollo, denominado Ponchoides, presenta la propuesta de la aplicación web Pardalis
                         como una solución para apoyar de manera integral a los estudiantes de primaria baja, con un enfoque
                         específico en aquellos que se encuentran en el rango de edades de 6 a 9 años, durante su proceso de
