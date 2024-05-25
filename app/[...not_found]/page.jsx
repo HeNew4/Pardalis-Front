f002e639c3009cd9a98ae210ed5e467a93b08611
PopeyedBird6 <isaac45604@gmail.com>
2024-05-25 00:21:32 -0600

    Botonsito

--- app/[...not_found]/page.jsx
+++ app/[...not_found]/page.jsx
@@ -20,7 +20,7 @@ export default function Custom404() {
         <p className="text-5xl font-bold text-black m-5">¡Vaya! algo salió mal.</p>
         <p className="mb-20">Por favor, intenta regresar al inicio.</p>
         <Link href='/'>
-          <motion.button className="focus:outline-none text-white bg-primary hover:bg-secundary focus:ring-4 focus:ring-secundary font-medium rounded-lg text-sm px-5 py-2.5 me-2 dark:focus:ring-primary m-3 w-9/12 mb-10" 
+          <motion.button className="focus:outline-none text-white fondo-especial2 hover:bg-secundary focus:ring-4 focus:ring-secundary font-medium rounded-lg text-sm px-5 py-2.5 me-2 dark:focus:ring-primary m-3 w-9/12 mb-10" 
           whileTap={{
                scale: 1.1,
                rotate: 5
