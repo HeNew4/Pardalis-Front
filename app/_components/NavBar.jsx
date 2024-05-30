a7c92d0074ad48cb069d47bd6eafabe2bba682e3
HeNew4 <angelmaclovio@yahoo.com>
2024-05-30 17:43:46 -0600

    Cambio de color

--- app/_components/NavBar.jsx
+++ app/_components/NavBar.jsx
@@ -32,7 +32,7 @@ export default function NavBar() {
                             height={50}
                         />
                     </motion.span>
-                    <span className='self-center text-2xl font-semibold whitespace-nowrap dark:text-white'>
+                    <span className='self-center text-2xl font-semibold whitespace-nowrap '>
                         Pardalis
                     </span>
                 </Link>
@@ -65,11 +65,11 @@ export default function NavBar() {
                     className='hidden w-full md:block md:w-auto'
                     id='navbar-default'
                 >
-                    <ul className='font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
+                    <ul className='font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0  md:dark:bg-gray-900 dark:border-gray-700'>
                         <li>
                             <Link
                                 href='/'
-                                className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary md:p-0 dark:text-white md:dark:hover:text-primary dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
+                                className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary md:p-0  md:dark:hover:text-primary dark:hover:bg-gray-700'
                             >
                                 Inicio
                             </Link>
@@ -77,7 +77,7 @@ export default function NavBar() {
                         <li>
                             <Link
                                 href='/adventure'
-                                className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary md:p-0 dark:text-white md:dark:hover:text-primary dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
+                                className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary md:p-0  md:dark:hover:text-primary dark:hover:bg-gray-700'
                             >
                                 Aventura
                             </Link>
@@ -85,7 +85,7 @@ export default function NavBar() {
                         <li>
                             <Link
                                 href='#'
-                                className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary md:p-0 dark:text-white md:dark:hover:text-primary dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
+                                className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary md:p-0  md:dark:hover:text-primary dark:hover:bg-gray-700'
                             >
                                 Perfil
                             </Link>
