048423c64881e0223cf86e41d33bd5d151145994
HeNew4 <angelmaclovio@yahoo.com>
2024-06-11 11:13:43 -0600

    Cambios

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,6 +1,18 @@
+'use client'
+
+import { useState } from "react";
+import NotLogin from "../_components/NotLogin";
+
 const { default: LayoutMain } = require("../_components/layoutMain");
 
 const Page = ({}) => {
+
+    const [login, setLogin] = useState(false)
+
+    if (!login) {
+        return <NotLogin />
+    }
+
     return (
         <LayoutMain>
             <div className="w-full max-w-sm min-w-60 p-5 bg-white border border-gray rounded-lg shadow">
@@ -11,17 +23,9 @@ const Page = ({}) => {
                         alt="Perfil"
                     />
                     <span className="text-sm text-gray-500 dark:text-gray-400">
-                        Nombre
+                        HeNew
                     </span>
                     
-                    <div className="flex mt-4 md:mt-6 justify-center">
-                        <a
-                            href="#"
-                            className="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-                        >
-                            Historial
-                        </a>
-                    </div>
                 </div>
             </div>
         </LayoutMain>
