d7e008f3def47c996c9628b29a4ef311f74b1197
HeNew4 <angelmaclovio@yahoo.com>
2024-06-11 19:22:07 -0600

    funcional

---
+++ app/_components/Alert.jsx
@@ -0,0 +1,23 @@
+function Alert() {
+    return (
+        <div className='content-alert'>
+            <div className='alerta'>
+                <div className='cabecera'>
+                    <div id='cerrar'></div>
+                </div>
+                <div className='contenido'>
+                    <div className='text'>
+                        Window
+                        <div className='line'></div>
+                        Ventana
+                    </div>
+                </div>
+                <div className='botones'>
+                    <div>:D</div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Alert;
--- app/adventure/world/2/page.jsx
+++ app/adventure/world/2/page.jsx
@@ -170,9 +170,6 @@ export default function Page({}) {
   const {playsound5} = st();
   const {playsound6} = sin();
 
-
-
-
   const [data, setData] = useState(_data);
 
   const handlerAreaClick = (area) => {
